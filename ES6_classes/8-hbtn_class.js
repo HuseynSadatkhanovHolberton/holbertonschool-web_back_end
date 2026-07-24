c055664b2cf1670b018a6dd02338167a4ff09676
HuseynSadatkhanovHolberton <12671@holbertonstudents.com>
2026-07-25 03:57:29 +0400

    es6classess
    donee

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,23 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return this._location;
+    }
+
+    // IMPORTANT: number + default cases
+    return this._size;
+  }
+}
